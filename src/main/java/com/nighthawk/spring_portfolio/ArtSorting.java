22c4b12e92b17eabf32a754a2b25d2271ec59301
vivianknee <vivianni452@gmail.com>
2023-11-30 21:42:34 -0800

    what

--- src/main/java/com/nighthawk/spring_portfolio/ArtSorting.java
+++ src/main/java/com/nighthawk/spring_portfolio/ArtSorting.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.art;
+package com.nighthawk.spring_portfolio;
 
 import java.util.Arrays;
 
--- src/main/java/com/nighthawk/spring_portfolio/mvc/art/Art.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/art/Art.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
