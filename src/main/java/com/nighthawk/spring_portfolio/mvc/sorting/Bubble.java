02774f873454c3b94254ddde8e865ec3ad508c76
aliya.tang <aliya.w.tang@gmail.com>
2023-12-05 03:27:22 -0800

    bubble

--- src/main/java/com/nighthawk/spring_portfolio/mvc/sorting/Bubble.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sorting/Bubble.java
@@ -7,12 +7,27 @@ import com.nighthawk.spring_portfolio.mvc.art.Art;
 public class Bubble extends Sorting {
     @Override
     public List<Art> sortArt(List<Art> unsortedArts) {
+        int n = unsortedArts.size();
+        Art temp;
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-i-1; j++){
+                if (unsortedArts.get(j+1).getLike() > unsortedArts.get(j).getLike()){
+                    //swap them if first one is less, should be GREATEST to LEAST order
+                    temp = unsortedArts.get(j);
+                    unsortedArts.set(j, unsortedArts.get(j));
+                    unsortedArts.set(j+1, temp);
+
+                }
+            }
+        }
+        /* 
         unsortedArts.sort(new Comparator<Art>() {
             @Override
             public int compare(Art o1, Art o2) {
                 return o1.getLike() - o2.getLike();
             }
         });
+        */
         return unsortedArts;
     }
 
